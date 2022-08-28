221df9f505f036777e9f20b8139b540f53afcad0
Mohd Saeed <mohd.saeed@oracle.com>
2022-08-28 19:34:06 +0530

    updated schema...

--- src/schema.js
+++ src/schema.js
@@ -35,8 +35,12 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in minutes"
-    length: Int
+    "The track's approximate length to complete, in seconds"
+    length: Int @deprecated(reasons: "use durationInSeconds")
+    
+    "The track's approximate length to complete, in seconds"
+    durationInSeconds: Int
+
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
